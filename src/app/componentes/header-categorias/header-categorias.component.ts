da7e03ce72321f09fe95cb32494023fda1c4eccc
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-07-01 06:58:35 -0300

    diseño del componente header categorias

--- src/app/componentes/header-categorias/header-categorias.component.ts
+++ src/app/componentes/header-categorias/header-categorias.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild  } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header-categorias',
@@ -6,20 +6,35 @@ import { Component, OnInit, ElementRef, ViewChild  } from '@angular/core';
   styleUrls: ['./header-categorias.component.css']
 })
 export class HeaderCategoriasComponent implements OnInit {
-
   @ViewChild('barraRef', { static: true }) barraRef!: ElementRef;
+  mostrarBotonIzquierdo = false;
 
   scrollIzquierda(): void {
     this.barraRef.nativeElement.scrollBy({ left: -100, behavior: 'smooth' });
+    this.toggleBotonDerecha(true); // Mostrar el botón derecho al hacer clic en el botón izquierdo
+
+    if (this.barraRef.nativeElement.scrollLeft === 0) {
+      this.mostrarBotonIzquierdo = false; // Ocultar el botón izquierdo al llegar al inicio del scroll
+    }
   }
 
   scrollDerecha(): void {
     this.barraRef.nativeElement.scrollBy({ left: 100, behavior: 'smooth' });
+    const maxScrollLeft = this.barraRef.nativeElement.scrollWidth - this.barraRef.nativeElement.clientWidth;
+    if (this.barraRef.nativeElement.scrollLeft >= maxScrollLeft) {
+      this.toggleBotonDerecha(false); // Ocultar el botón derecho al llegar al final del scroll
+    }
+    this.mostrarBotonIzquierdo = true; // Mostrar el botón izquierdo al desplazarse hacia la derecha
   }
 
-  constructor() { }
+  toggleBotonDerecha(show: boolean): void {
+    const botonDerecha = document.querySelector('.boton-derecha') as HTMLElement;
+    botonDerecha.style.display = show ? 'block' : 'none';
+  }
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.mostrarBotonIzquierdo = false; // Ocultar el botón izquierdo inicialmente
   }
-
 }
