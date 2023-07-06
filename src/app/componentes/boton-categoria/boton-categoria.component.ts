95b990b9d245b0b8db00c11afe2d03d7078a62dc
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-07-05 23:28:57 -0300

    fix(componente boton): dataBinding para las categorias agregue más categorias

--- src/app/componentes/boton-categoria/boton-categoria.component.ts
+++ src/app/componentes/boton-categoria/boton-categoria.component.ts
@@ -78,6 +78,444 @@ export class BotonCategoriaComponent implements OnInit {
       vistas: "6K",
       tiempo: "8 horas",
       categoria: "programación"  
+    },    
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
+    },    
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
+    },    
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
+    },
+        { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
+    },    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
+    },    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
+    },    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/T_j60n1zgu0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYfR8pCzSqLuhn1ydiQfaMtpJoKA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Reaccionando a  las Tecnologías y Lenguajes de Programación Mejor Pagadas en 2023',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación" 
+    },
+    { 
+      vistaPrevia: 'https://i.ytimg.com/vi/RCih9bUkH1w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDT7u0xKlmShngU9Lkjylh2NnXKaA', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqPAmx41p_AxeAw-d61aVutp2zNuOe3CMHLDtxrC=s68-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"VVLP",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "programación"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "viajes"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "diseño gráfico"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "Música"  
+    },
+    { 
+      vistaPrevia: 'https://via.placeholder.com/500x281?text=placeholder', 
+      avatar: "https://yt3.ggpht.com/ytc/AGIKgqMSpqpZVn-TGZerYSVeuzhBqjNLU8f9_XYVMfOP=s48-c-k-c0x00ffffff-no-rj", 
+      nombre: 'Nombre del video',
+      usuario:"midudev",
+      vistas: "6K",
+      tiempo: "8 horas",
+      categoria: "En tiempo real"  
     }
   ];
 
