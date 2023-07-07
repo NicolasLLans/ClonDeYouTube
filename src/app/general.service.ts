ade53ea3cc07d2f95563194acfcf146293ca74ab
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-07-06 21:18:51 -0300

    fix(Service general):servicio general para hacer el fetch de datos de db.json

--- src/app/general.service.ts
+++ src/app/general.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class GeneralService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  obtenerDatos():any{
+    this.http.get('src/assets/db.json')
+  }
 }
